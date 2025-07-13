2dd011340e249b5ff795f463d841fa8e5f07b913
Tony Richards <trichards57@pm.me>
2025-07-13 19:51:03 +0100

    feat: adds identity database
    Signed-off-by: Tony Richards <trichards57@pm.me>

---
+++ api/IdentityDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Daedalus.Api
+{
+    public class IdentityDataContext(DbContextOptions<IdentityDataContext> options) : IdentityDbContext(options)
+    {
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,23 @@
+using Daedalus.Api;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<IdentityDataContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresIdentity")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<IdentityDataContext>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
